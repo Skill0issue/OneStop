e3d0871412175feb81ab7d5dc42da2185687c95d
Nihar Kartikeya <127834591+Skill0issue@users.noreply.github.com>
2024-06-29 02:24:36 +0530

    added selection to canvas Elements

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect, useState, useRef } from "react";
 import useCanvas from "../hooks/useCanvas";
 import rough from "roughjs/bundled/rough.esm";
 import { RoughCanvas } from "roughjs/bin/canvas";
@@ -7,52 +7,133 @@ const Canvas: React.FC = () => {
   const canvasRef = useCanvas();
   const generator = rough.generator();
 
-  const [drawType, setDrawType] = useState("rectangle"); // Default to rectangle drawing
-  const [drawing, setDrawing] = useState(false);
-  const [elements, setElements] = useState<any[]>([]);
-  const [elementType, setElementType] = useState("normal"); // rough or normal
+  const [toolType, setToolType] = useState<string>("rectangle");
+  const [elementType, setElementType] = useState<string>("normal");
+  const [action, setAction] = useState<string>("none");
+
+  const [elements, setElements] = useState<element[]>([]);
+  const [selectedElement, setSelectedElement] = useState<element | null>(null);
+  const [selectedElementOffset, setSelectedElementOffset] = useState<{
+    offsetX: number;
+    offsetY: number;
+  }>({ offsetX: 0, offsetY: 0 });
+
+  type element = {
+    toolType: string;
+    elementType: string;
+    id: number;
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+  };
+
+  const distance = (
+    a: { x: number; y: number },
+    b: { x: number; y: number }
+  ): number => {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+  };
 
   const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    setDrawing(true);
     const { clientX, clientY } = event;
+      if (toolType === "select") {
+      const currentElement: element | undefined = elements.find((el) => {
+        const { toolType } = el;
+        if (toolType === "line") {
+          const a = { x: el.x1, y: el.y1 };
+          const b = { x: el.x2, y: el.y2 };
+          const c = { x: clientX, y: clientY };
+          const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+          return Math.abs(offset) < 1;
+        }
+        if (toolType === "rectangle") {
+          const minX = Math.min(el.x1, el.x2);
+          const minY = Math.min(el.y1, el.y2);
+          const maxX = Math.max(el.x1, el.x2);
+          const maxY = Math.max(el.y1, el.y2);
 
-    // Create an element with the draw type
-    setElements((prev) => [
-      ...prev,
-      {
-        drawType: drawType,
-        elementType: elementType,
-        x1: clientX,
-        y1: clientY,
-        x2: clientX,
-        y2: clientY,
-      },
-    ]);
+          return (
+            clientX <= maxX &&
+            clientY <= maxY &&
+            clientX >= minX &&
+            clientY >= minY
+          );
+        }
+        return false;
+      });
+      if (currentElement) {
+        setAction("moving");
+        setSelectedElement(currentElement);
+        setSelectedElementOffset({
+          offsetX: clientX - currentElement.x1,
+          offsetY: clientY - currentElement.y1,
+        });
+      }
+    } else { // @TODO : need to change this when adding resize and other options :(
+      setElements((prev) => [
+        ...prev,
+        {
+          toolType: toolType,
+          elementType: elementType,
+          x1: clientX,
+          y1: clientY,
+          x2: clientX,
+          y2: clientY,
+          id: elements.length,
+        },
+      ]);
+      setAction("drawing");
+    }
   };
 
   const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const { clientX, clientY } = event;
-    if (!drawing) {
-      return;
-    }
+      if (action === "moving" && selectedElement) {
+          (event.target as HTMLCanvasElement).style.cursor = "move";
+      const { offsetX, offsetY } = selectedElementOffset;
+      const width = selectedElement.x2 - selectedElement.x1;
+      const height = selectedElement.y2 - selectedElement.y1;
 
-    // Update the elements
-    setElements((prev) => {
-      const updatedElements = [...prev];
-      const length = updatedElements.length;
-      if (length > 0) {
-        updatedElements[length - 1] = {
-          ...updatedElements[length - 1],
-          x2: clientX,
-          y2: clientY,
-        };
-      }
-      return updatedElements;
-    });
+      const newX1 = clientX - offsetX;
+      const newY1 = clientY - offsetY;
+
+      const updatedElement = {
+        ...selectedElement,
+        x1: newX1,
+        y1: newY1,
+        x2: newX1 + width,
+        y2: newY1 + height,
+      };
+
+      setSelectedElement(updatedElement);
+      setElements((prev) => {
+        const index = prev.findIndex((el) => el.id === selectedElement.id);
+        const updatedElements = [...prev];
+        updatedElements[index] = updatedElement;
+        return updatedElements;
+      });
+    }
+    if (action === "drawing") {
+      setElements((prev) => {
+        const updatedElements = [...prev];
+        const length = updatedElements.length;
+        if (length > 0) {
+          updatedElements[length - 1] = {
+            ...updatedElements[length - 1],
+            x2: clientX,
+            y2: clientY,
+          };
+        }
+        return updatedElements;
+      });
+    }
   };
 
-  const handleMouseUp = () => {
-    setDrawing(false);
+  const handleMouseUp = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    setAction("none");
+    setSelectedElement(null);
+    (event.target as HTMLCanvasElement).style.cursor = "default";
   };
 
   const draw = (
@@ -60,10 +141,10 @@ const Canvas: React.FC = () => {
     context: CanvasRenderingContext2D | null
   ) => {
     if (context) {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear canvas before drawing
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
       elements.forEach((elem) => {
-        switch (elem.drawType) {
+        switch (elem.toolType) {
           case "line":
             if (elem.elementType === "normal") {
               context.strokeStyle = "black";
@@ -116,7 +197,7 @@ const Canvas: React.FC = () => {
         draw(roughCanvas, context);
       }
     }
-  }, [canvasRef, elements]); // Redraw when canvasRef or elements change
+  }, [canvasRef, elements]);
 
   return (
     <>
@@ -134,19 +215,26 @@ const Canvas: React.FC = () => {
         </select>
         <input
           type="radio"
-          name="drawType"
+          name="toolType"
           id="line"
-          checked={drawType === "line"}
-          onChange={() => setDrawType("line")}
+          checked={toolType === "line"}
+          onChange={() => setToolType("line")}
         />
         <label htmlFor="line">Line</label>
-
         <input
           type="radio"
-          name="drawType"
+          name="toolType"
+          id="select"
+          checked={toolType === "select"}
+          onChange={() => setToolType("select")}
+        />
+        <label htmlFor="select">Select</label>
+        <input
+          type="radio"
+          name="toolType"
           id="rectangle"
-          checked={drawType === "rectangle"}
-          onChange={() => setDrawType("rectangle")}
+          checked={toolType === "rectangle"}
+          onChange={() => setToolType("rectangle")}
         />
         <label htmlFor="rectangle">Rectangle</label>
       </div>
@@ -155,7 +243,7 @@ const Canvas: React.FC = () => {
         id="canvas"
         width={window.innerWidth}
         height={window.innerHeight}
-        className=""
+        className={``}
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
